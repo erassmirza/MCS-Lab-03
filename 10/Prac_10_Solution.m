d1b7652dd526e9fa2d92b523f963ca1a7f1e3787
erassmirza <erass.mirza@gmail.com>
2020-09-11 01:30:34 +0500

    Feedback of Closed Loop System

--- 10/Prac_10_Solution.m
+++ 10/Prac_10_Solution.m
@@ -1,35 +1,28 @@
 clear, clc, close
-num = 1/705;
-den = [1 30/705 15/705];
-plant = tf(num,den);
+num = 1/50;
+den = [1 10/50 5/50];
+plant = tf(num,den)
 step(plant);
-Kp=200; Ki=1; Kd=1;
+
+Kp=500; Ki=10; Kd=100;
 contr=tf([Kd Kp Ki],[1 0]);
 sys_cl=feedback(contr*plant,1)
-t=0:0.01:300;
-step(sys_cl,t);
-[wn,zeta,p] = damp(sys_cl)
+t=0:0.0001:300;
 
-zeta = zeta(2);
-wn = wn(2);
-n=wn*wn;
-d=[1 2*zeta*wn wn*wn];
-disp('The transfer function is: ')
-printsys(n,d);
-t=0:0.01:300;
+figure
+[n,d] = tfdata(sys_cl)
+step(n,d,t);
 [y,x,t]=step(n,d,t);
-plot(t,y);
-grid on;
-title('step response');
+
 %to find rise time i.e. time taken for output to rise from
 %10% to 90%
 k=1;
 while y(k)<=0.1;
-k=k+1;
+    k=k+1;
 end
 tenpercent=t(k);
 while y(k)<=0.9;
-k=k+1;
+    k=k+1;
 end
 nintypercent=t(k);
 rtime=nintypercent-tenpercent;
@@ -38,18 +31,18 @@ format short
 % to find delay time i.e. time taken to rise to 50% of step
 k=1;
 while y(k)<=0.5;
-k=k+1;
+    k=k+1;
 end
 dtime=t(k);
 fprintf('The delay time is: %f sec\n', dtime);
 % to find maximum overshoot
 for k=1:length(t)-1;
-if y(k+1)<=y(k);
-% to find value of k till response keeps rising
-break;
+    if y(k+1)<=y(k);
+        % to find value of k till response keeps rising
+        break;
+    end;
 end;
-end;
-Oshoot=y(k)-y(length(t)-1);
+Oshoot=y(k)-1;
 fprintf('The overshoot is: %f sec\n', Oshoot);
 % to find the peak time
 tp=t(k);
@@ -60,11 +53,10 @@ fprintf('the peak time is :%f sec\n',tp)
 %2%
 tol=0.02;
 for k=length(t)-1:-1:2;
-if(abs(y(k)-y(length(t)-1))>tol)
-break;
-end;
+    if(abs(y(k)-y(length(t)-1))>tol)
+        break;
+    end;
 end;
 stime=t(k); 
 fprintf('the settling time is :%f sec\n',stime)
 sserror = abs(1-y(end))
-
